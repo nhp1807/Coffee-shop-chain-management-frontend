592dce2b551871bc9ef22395e903bf898408d8fb
Phong Nguyen <nhp1807@gmail.com>
2024-12-19 09:27:19 +0700

    Update: update base_url_fe

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 // const BASE_URL = "https://coffee-shop-chain-management.onrender.com";
 const BASE_URL = "http://localhost:8080";
 
-const BASE_URL_FRONTEND = "http://localhost:3000/Coffee-shop-chain-management-frontend#";
+// const BASE_URL_FRONTEND = "http://localhost:3000/Coffee-shop-chain-management-frontend#";
+const BASE_URL_FRONTEND = "https://nhp1807.github.io/Coffee-shop-chain-management-frontend/#";
 
 export { BASE_URL, BASE_URL_FRONTEND };
 
